5e1d2143012c9cd9a6acc6bbd329831e1e81d933
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-08 22:07:21 +0900

    heroku

--- alcohol_trip/settings.py
+++ alcohol_trip/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG") == "True"
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 if DEBUG: 
     MEDIA_URL = "/media/"
     MEDIA_ROOT = BASE_DIR / "media"
